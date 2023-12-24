0563f0a9d128a0c6a0c1d42d22760ad9a995c824
bheemuk123 <bheemuk123@gmail.com>
2023-12-24 12:04:15 +0530

    added prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  spoonacularApiKey : '35549cddc59547a4a77117bf5e718612',
+  spoonacularBaseUrl : 'https://recipe-management-siif.onrender.com',
+  userRegisterUrl:"https://recipe-management-siif.onrender.com/user/register",
+  userLoginUrl:"https://recipe-management-siif.onrender.com/user/login",
+  updateRecent:"https://recipe-management-siif.onrender.com/user/updateRecent",
+  fetchRecent:"https://recipe-management-siif.onrender.com/user/fetchRecent",
+  fetchFavorites:"https://recipe-management-siif.onrender.com/user/fetchFavorite",
+  addToFavorite:"https://recipe-management-siif.onrender.com/user/AddToFavorite",
+  addToMyRecipes:"https://recipe-management-siif.onrender.com/user/myRecipe",
+  fetchMyRecipes:"https://recipe-management-siif.onrender.com/user/fetchMyRecipes",
+  email:'bheemuk123@gmail.com',
+  contact:'+91-7406604654',
+  version:'1.0.0'
 };
